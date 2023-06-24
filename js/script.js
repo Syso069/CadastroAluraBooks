ebad0f5ab4d932630b04d2674f4ea00496699fc0
Syso <sisomd69@gmail.com>
2023-06-24 10:23:52 -0300

    add catch e throw

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
-var consultaCep = fetch("https://viacep.com.br/ws/01001000/json");
-
+var consultaCep = fetch("https://viacep.com.br/ws/01001000/json")
+    .then(resposta => resposta.json())
+    .then(r => {
+        if(r.error) {
+            throw Error ('Esse cep não existe!');
+        } else {
+            console.log(r);
+        }
+    })
+    .catch(error => console.log(error))
+    .finally(mensagem => console.log('Processamento concluído!'));
+    
 console.log(consultaCep);
